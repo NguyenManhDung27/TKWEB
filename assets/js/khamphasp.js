cbdd2f4eb7ff9d76bc1d59eafcf91e0ccaf1be41
Phạm Thu Hà <thuha0307.pth@gmail.com>
2025-06-05 14:10:29 +0700

    Update khamphasp.js

--- assets/js/khamphasp.js
+++ assets/js/khamphasp.js
@@ -1,351 +1,51 @@
-const bestSellingProducts = [
-  {
-    name: "Tranh Tráng Gương Đôi Hươu",
-    img: "assets/img/gia-tranh-trang-guong-2.jpg",
-    price: "750.000đ",
-    oldPrice: "1.150.000đ"
-  },
+(function () {
+    const images = [
+   
+        './assets/img/tranhsen2.png',
+        './assets/img/tranhsen3.png',
+        './assets/img/tranhsen4.png',
+        './assets/img/tranhsen5.png',
+        './assets/img/tranhsen6.png',
+        './assets/img/tranhsen8.png',
+        './assets/img/tranhsen1.png',
+        './assets/img/tranhsen7.png',
+    ];
+    const gallery = document.getElementById('gallery'); 
 
-  {
-    name: "Tranh Tứ Quý Xuân Hạ Thu Đông",
-    img: "assets/img/tranhtuquy11.jpg",
-    price: "700.000đ",
-    oldPrice: "1.200.000đ"
-  },
-  {
-    name: "Tranh Bộ 3 Phong Cảnh",
-    img: "assets/img/tranhbophongcanh.jpg",
-    price: "980.000đ",
-    oldPrice: "1.430.000đ"
-  },
-  {
-    name: "Tranh Bán Nguyệt Hoa Sen",
-    img: "assets/img/tranh-ban-nguyet-23470115.jpg",
-    price: "650.000đ",
-    oldPrice: "900.000đ"
-  },
-  {
-    name: "Tranh Tráng Gương Phong Cảnh",
-    img: "assets/img/O1CN01YhP0Jt1NLJr1vo4ca_2207958381553-min.jpg",
-    price: "1.200.000đ",
-    oldPrice: "1.340.000đ"
-  },
-  {
-    name: "Tranh Mã Đáo Thành Công",
-    img: "assets/img/4945b1e6926c3a32637d.jpg",
-    price: "999.000đ",
-    oldPrice: "1.400.000đ"
-  },
-  {
-    name: "Tranh Tráng Gương Lá Bạch Quả",
-    img: "assets/img/Tranh-Hinh-Tron-Trang-Guong-3D-La-Bach-Qua-Va-2-Chu-Huou-TTG076.jpg",
-    price: "890.000đ",
-    oldPrice: "1.120.000đ"
-  },
-  {
-    name: "Tranh Đèn Led Cá Và Sen",
-    img: "assets/img/tranh-den-led-phong-khach-ca-va-sen.jpg",
-    price: "710.000đ",
-    oldPrice: "900.000đ"
-  },
-  {
-    name: "Tranh Tráng Gương Đôi Công",
-    img: "assets/img/tranh-trang-guong-8.jpg",
-    price: "750.000đ",
-    oldPrice: "980.000đ"
-  },
-  {
-    name: "Tranh Đồng Hồ Sơn Thuỷ",
-    img: "assets/img/Tranh đồng hô1.webp",
-    price: "500.000đ",
-    oldPrice: "740.000đ"
-  }
-];
+    images.forEach((src, index) => {
+        const card = document.createElement('div');
+        card.className = 'card';
 
+        const img = document.createElement('img');
+        img.src = src;
+        img.alt = 'Image ' + (index + 1);
 
-const products = [
-  { name: "Tranh Tráng Gương Sơn Vân", img: "./assets/img/kpt.tranhtrangguong4.webp", price: "980.000đ", category: "trang-guong" },
-  { name: "Tranh Đèn Led Hoa", img: "./assets/img/kpt.tranhdenled5.webp", price: "710.000đ", category: "den-led" },
-  { name: "Tranh Tam Cốc Mùa Lúa", img: "./assets/img/kpt.tranhphongcanhr5.jpeg", price: "700.000đ", category: "chu-de" },
-  { name: "Tranh Bộ Huyền Dạ", img: "./assets/img/kpt.tranhbo1.jpg", price: "750.000đ", category: "trang-guong" },
-  { name: "Tranh Đồng Hồ Đôi Hươu Tài Lộc", img: "./assets/img/kpt.tranhdongho4.webp", price: "650.000đ", category: "dong-ho" },
-  { name: "Tranh Ngư Thuỷ", img: "./assets/img/kpt.tranhbannguyet5.jpeg", price: "600.000đ", category: "trang-guong" },
-  { name: "Tranh Canvas Hoa", img: "./assets/img/kpt.tranhcanvas4.jpg", price: "600.000đ", category: "canvas" },
-  { name: "Tranh Bán Nguyệt Hươu Lộc", img: "./assets/img/kpt.tranhbannguyet4.webp", price: "750.000đ", category: "trang-guong" },
-  { name: "Tranh Đồng Hồ Lộc Nguyệt", img: "./assets/img/kpt.tranhdongho3.webp", price: "620.000đ", category: "dong-ho" },
-  { name: "Tranh Canvas Thanh Hoa", img: "./assets/img/kpt.tranhcanvas3.jpg", price: "430.000đ", category: "canvas" },
-  { name: "Tranh Tráng Gương Cá Và Sen", img: "./assets/img/kpt.tranhtrangguong3.jpg", price: "880.000đ", category: "trang-guong" },
-  { name: "Tranh Sơn Dầu Xanh Trong Trẻo", img: "./assets/img/kpt.tranhsondau3.jpg", price: "255.000đ", category: "son-dau" },
-  { name: "Tranh Canvas Hoa Trắng", img: "./assets/img/kpt.tranhcanvas1.jpg", price: "399.000đ", category: "canvas" },
-  { name: "Tranh Dải Ngân Sơn", img: "./assets/img/kpt.tranhphongcanh6.jpeg", price: "770.000đ", category: "chu-de" },
-  { name: "Tranh Đèn Led Cá Và Sen", img: "./assets/img/kpt.tranhdenled4.webp", price: "865.000đ", category: "den-led" },
-  { name: "Tranh Sơn Dầu Phố Cổ Hà Nội", img: "./assets/img/kpt.tranhsondau4.jpg", price: "235.000đ", category: "son-dau" },
-  { name: "Tranh Thuận Buồm Xuôi Gió", img: "./assets/img/kpt.tranhchudetbxg.jpg", price: "520.000đ", category: "chu-de" },
-  { name: "Tranh Tứ Quý Xuân Hạ Thu Đông", img: "./assets/img/tranhtuquy1.jpg", price: "300.000đ", category: "trang-guong" },
-  { name: "Tranh Đèn Led Sơn Lộc", img: "./assets/img/kpt.tranhdenled3.webp", price: "450.000đ", category: "den-led" },
-  { name: "Tranh Núi Non", img: "./assets/img/kpt.tranhphongcanh3.jpeg", price: "450.000đ", category: "phong-canh" },
-  { name: "Tranh Sơn Thuỷ Hữu Tình", img: "./assets/img/kpt.sonthuy1.jpeg", price: "400.000đ", category: "phong-canh" },
-  { name: "Tranh Bộ Năm Mùa Thu", img: "./assets/img/kpt.bo5.jpeg", price: "380.000đ", category: "trang-guong" },
-  { name: "Tranh Đèn Led Cá Và Trăng", img: "./assets/img/kpt.tranhdenled1.webp", price: "480.000đ", category: "den-led" },
-  { name: "Tranh Phong Cảnh Nhật Bản", img: "./assets/img/kpt.tranhphongcanh2.jpeg", price: "510.000đ", category: "phong-canh"
-    
-
-
-
-
-   },
-];
-
-let currentPage = 1;
-const itemsPerPage = 12;
-let currentCategory = "all";
-
-function renderProducts() {
-  const container = document.getElementById("explore-products");
-  if (!container) return;
-
-  const filtered = currentCategory === "all"
-    ? products
-    : products.filter(p => p.category === currentCategory);
-
-  const paginated = filtered.slice(
-    (currentPage - 1) * itemsPerPage,
-    currentPage * itemsPerPage
-  );
-
-container.innerHTML = paginated.map((p, index) => `
-  <div class="col-md-3 mb-4">
-    <div class="card text-white">
-      <a href="pages/detail.html?index=${index + (currentPage - 1) * itemsPerPage}" style="text-decoration:none; color:inherit;">
-        <img src="${p.img}" class="card-img" alt="${p.name}" style="height: 250px; object-fit: cover;">
-        <div class="card-img-overlay d-flex flex-column justify-content-end bg-dark bg-opacity-50 p-3 rounded">
-          <h5 class="card-title">${p.name}</h5>
-          <p class="card-text fw-bold">${p.price}</p>
-          <a href="pages/detail.html?index=${index + (currentPage - 1) * itemsPerPage}" class="btn btn-sm btn-light">Xem chi tiết</a>
-        </div>
-      </a>
-    </div>
-  </div>
-`).join("");
-
-
-
-
-  renderPagination(filtered.length);
-}
-
-function renderPagination(totalItems) {
-  const pagination = document.getElementById("pagination");
-  if (!pagination) return;
-
-  const totalPages = Math.ceil(totalItems / itemsPerPage);
-  pagination.innerHTML = "";
-
-  for (let i = 1; i <= totalPages; i++) {
-    pagination.innerHTML += `
-      <li class="page-item ${i === currentPage ? "active" : ""}">
-        <button class="page-link" onclick="goToPage(${i})">${i}</button>
-      </li>
-    `;
-  }
-}
-
-function goToPage(page) {
-  currentPage = page;
-  renderProducts();
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  renderProducts();
-  renderBestSellingProducts();
-
-  const categoryFilter = document.getElementById("category-filter");
-  if (categoryFilter) {
-    categoryFilter.addEventListener("click", (e) => {
-      if (e.target.dataset.category) {
-        document.querySelectorAll("#category-filter .btn").forEach(btn =>
-          btn.classList.remove("active")
-        );
-        e.target.classList.add("active");
-        currentCategory = e.target.dataset.category;
-        currentPage = 1;
-        renderProducts();
-      }
+        card.appendChild(img);
+        gallery.appendChild(card);
     });
-  }
-});
-document.addEventListener("DOMContentLoaded", function () {
-  const searchInput = document.getElementById("searchInput");
-  const searchIcon = document.getElementById("searchIcon");
 
-  if (!searchInput || !searchIcon) return;
+    // Hiệu ứng nghiêng khi di chuyển chuột
+    const cards = gallery.querySelectorAll('.card'); 
 
-  searchIcon.addEventListener("click", handleSearch);
-  searchInput.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") handleSearch();
-  });
+    cards.forEach(card => {
+        card.addEventListener('mousemove', (e) => {
+            const rect = card.getBoundingClientRect();
+            const width = rect.width;
+            const height = rect.height;
+            const centerX = rect.left + width / 2;
+            const centerY = rect.top + height / 2;
+            const mouseX = e.clientX - centerX;
+            const mouseY = e.clientY - centerY;
 
-  function handleSearch() {
-    const keyword = searchInput.value.trim().toLowerCase();
-    filterProductsByKeyword(keyword);
-    filterBestSellingByKeyword(keyword);
+            const rotateX = Math.max(Math.min((mouseY / height) * 75, 75), -75);
+            const rotateY = Math.max(Math.min((mouseX / width) * -75, 75), -75);
 
-  }
-  function filterBestSellingByKeyword(keyword) {
-    const matched = bestSellingProducts.filter(p =>
-      p.name.toLowerCase().includes(keyword)
-    );
-    renderBestSellingProducts(matched);
-  }
-  
-  function filterProductsByKeyword(keyword) {
-    const container = document.getElementById("explore-products");
-    if (!container) return;
+            card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        });
 
-    const matchedProducts = products.filter(p =>
-      p.name.toLowerCase().includes(keyword)
-    );
-
-    const paginated = matchedProducts.slice(0, itemsPerPage); // chỉ trang đầu
-
-    container.innerHTML = paginated.map(p => `
-      <div class="col-md-3 mb-4">
-        <div class="card text-white">
-          <img src="${p.img}" class="card-img" alt="${p.name}" style="height: 250px; object-fit: cover;">
-          <div class="card-img-overlay d-flex flex-column justify-content-end bg-dark bg-opacity-50 p-3 rounded">
-            <h5 class="card-title">${p.name}</h5>
-            <p class="card-text fw-bold">${p.price}</p>
-            <a href="#" class="btn btn-sm btn-light">Mua ngay</a>
-          </div>
-        </div>
-      </div>
-    `).join("");
-
-    renderPagination(matchedProducts.length);
-    currentCategory = "all"; // reset lại lọc danh mục nếu có
-  }
-});
-function renderBestSellingProducts(productsToRender = bestSellingProducts) {
-  const carouselInner = document.querySelector("#flashSaleCarousel .carousel-inner");
-  if (!carouselInner) return;
-
-  carouselInner.innerHTML = "";
-
-  const chunkSize = 5;
-  for (let i = 0; i < productsToRender.length; i += chunkSize) {
-    const items = productsToRender.slice(i, i + chunkSize);
-
-    const cardsHtml = items.map(p => `
-      <div class="card me-3" style="width: 18rem;">
-        <img src="${p.img}" class="card-img-top" alt="${p.name}">
-        <div class="card-body text-center">
-          <h5 class="card-title">${p.name}</h5>
-          <p class="fw-bold text-brown custom-font">${p.price} <small class="text-muted text-decoration-line-through">${p.oldPrice}</small></p>
-          <a href="#" class="btn btn-sm btn-danger">Mua ngay</a>
-        </div>
-      </div>
-    `).join("");
-
-    carouselInner.innerHTML += `
-      <div class="carousel-item ${i === 0 ? "active" : ""}">
-        <div class="d-flex justify-content-between">
-          ${cardsHtml}
-        </div>
-      </div>
-    `;
-  }
-}
-// pop-up//
-
-const popup = document.getElementById('popupDetail');
-const popupContent = popup.querySelector('.popup-content');
-const popupImg = document.getElementById('popupImg');
-const popupTitle = document.getElementById('popupTitle');
-const popupDesc = document.getElementById('popupDesc');
-const popupRating = document.getElementById('popupRating');
-const popupPurchases = document.getElementById('popupPurchases');
-const sizeButtons = document.querySelectorAll('.size-btn');
-const quantityInput = document.getElementById('quantityInput');
-const totalPriceElement = document.getElementById('totalPrice');
-
-let selectedSize = null;
-let priceValue = 0;
-let quantity = 1;
-
-document.querySelectorAll('.buy-now').forEach(button => {
-    button.addEventListener('click', () => {
-        const card = button.closest('.col-6, .col-md-3');
-        const imgSrc = card.querySelector('img').src;
-        const title = card.querySelector('.fw-semibold').innerText;
-        const price = card.querySelector('.text-danger').innerText;
-        const description = card.querySelector('.product-data .description').textContent;
-        const rating = card.querySelector('.product-data .rating').textContent;
-        const purchases = card.querySelector('.product-data .purchases').textContent;
-
-        popupImg.src = imgSrc;
-        popupTitle.textContent = title;
-        popupDesc.textContent = description;
-        popupRating.innerHTML = `<i class="fas fa-star"></i> ${rating}`;
-        popupPurchases.textContent = purchases;
-
-        priceValue = parseInt(price.replace(/[^0-9]/g, ''));
-        quantity = 1;
-        quantityInput.value = quantity; // Cập nhật giá trị số lượng
-        updateTotalPrice();
-
-        sizeButtons.forEach(btn => btn.classList.remove('active'));
-        selectedSize = null;
-
-        popup.style.display = 'flex';
-        setTimeout(() => popup.classList.add('show'), 10);
+        card.addEventListener('mouseleave', () => {
+            card.style.transition = 'transform 0.2s ease';
+            card.style.transform = 'perspective(1000px) rotateX(0) rotateY(0)';
+        });
     });
-});
-
-sizeButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        sizeButtons.forEach(btn => btn.classList.remove('active'));
-        button.classList.add('active');
-        selectedSize = button.getAttribute('data-size');
-    });
-});
-
-function updateQuantity(change) {
-    quantity += change;
-    if (quantity < 1) quantity = 1;
-    quantityInput.value = quantity; // Cập nhật giá trị trong ô input
-    updateTotalPrice();
-}
-
-function updateTotalPrice() {
-    const total = priceValue * quantity;
-    totalPriceElement.textContent = total.toLocaleString('vi-VN') + '₫';
-}
-
-function closePopup() {
-    popup.classList.remove('show');
-    setTimeout(() => {
-        popup.style.display = 'none';
-    }, 300);
-}
-
-document.querySelector('.buy-now-popup').addEventListener('click', () => {
-    if (!selectedSize) {
-        alert('Vui lòng chọn kích thước trước khi mua!');
-        return;
-    }
-    alert(`Bạn đã chọn mua ngay sản phẩm: ${popupTitle.textContent} - Kích thước: ${selectedSize} - Số lượng: ${quantity} - Thành tiền: ${totalPriceElement.textContent}. Chuyển đến trang thanh toán!`);
-    closePopup();
-});
-
-document.querySelector('.add-to-cart-popup').addEventListener('click', () => {
-    if (!selectedSize) {
-        alert('Vui lòng chọn kích thước trước khi thêm vào giỏ hàng!');
-        return;
-    }
-    alert(`Sản phẩm: ${popupTitle.textContent} - Kích thước: ${selectedSize} - Số lượng: ${quantity} - Thành tiền: ${totalPriceElement.textContent} đã được thêm vào giỏ hàng!`);
-    closePopup();
-});
-
-popup.addEventListener('click', e => {
-    if (e.target === popup) closePopup();
-});
+})();
